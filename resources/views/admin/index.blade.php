48d96c8f4ad84601885874258a695d356e553afb
Bill <bill80362@gmail.com>
2023-08-21 21:48:59 +0800

    v1

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -24,15 +24,103 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-                    <div class="card card-primary">
-                        <div class="card-header text-center">
-                            <div class="d-flex justify-content-between">
-                                <h3 class="card-title">列表</h3>
-{{--                                <a>View Report</a>--}}
-                            </div>
-                        </div>
+                    <div class="card card-primary card-outline">
+{{--                        <div class="card-header text-center">--}}
+{{--                            <div class="d-flex justify-content-between">--}}
+{{--                                <h3 class="card-title">列表</h3>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
                         <div class="card-body">
-                            Page內文
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <label>狀態</label>
+                                    <select class="form-control">
+                                        <option>不限制</option>
+                                        <option>正式</option>
+                                        <option>非正式</option>
+                                    </select>
+                                </div>
+                                <div class="col-lg-4">
+                                    <label>文字條件搜尋</label>
+                                    <div class="form-group">
+                                        <div class="input-group input-group">
+                                            <div class="input-group-prepend">
+                                                <select class="form-control">
+                                                    <option>不限制</option>
+                                                    <option>姓名</option>
+                                                    <option>ID</option>
+                                                    <option>手機</option>
+                                                    <option>Email</option>
+                                                    <option>地址</option>
+                                                </select>
+                                            </div>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <label>排序</label>
+                                    <select class="form-control">
+                                        <option>建立時間(反)</option>
+                                        <option>建立時間(正)</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="card card-primary">
+                                        <div class="card-header">
+                                            <div class="card-title">
+                                                <button class="btn btn-sm btn-warning">全選</button>
+                                                <button class="btn btn-sm btn-warning">取消</button>
+                                                <button class="btn btn-sm btn-danger">勾選刪除</button>
+                                            </div>
+                                            <div class="card-tools">
+                                                <button class="btn btn-sm btn-warning">更新排序</button>
+                                                <button class="btn btn-sm btn-warning">匯入</button>
+                                                <button class="btn btn-sm btn-warning">匯出</button>
+                                            </div>
+                                        </div>
+                                        <div class="card-body table-responsive p-0">
+                                            <table class="table table-hover">
+                                                <thead>
+                                                <tr>
+                                                    <th>ID</th>
+                                                    <th>排序</th>
+                                                    <th>姓名</th>
+                                                    <th>生日</th>
+                                                    <th>手機</th>
+                                                    <th>Email</th>
+                                                    <th>狀態</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                <tr>
+                                                    <td>
+                                                        <input type="checkbox" class="form-check-input">
+                                                        183
+                                                    </td>
+                                                    <td>
+                                                        <input class="form-control form-control-sm" type="text" size="1">
+                                                    </td>
+                                                    <td>John Doe</td>
+                                                    <td>2014-11-7</td>
+                                                    <td>0911222333</td>
+                                                    <td>bill@gmail.com</td>
+                                                    <td>正式</td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            </div>
+
+
+
+
                         </div>
                     </div>
                 </div>
