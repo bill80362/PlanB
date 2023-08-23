b1bffc4c7130e25d79921639d8d3220cdf732494
bill <bill80362@gmail.com>
2023-08-23 18:28:26 +0800

    Member_Data

--- app/Models/Member/Member_Data.php
+++ app/Models/Member/Member_Data.php
@@ -21,8 +21,22 @@ class Member_Data extends Model
     //
     protected $guarded = [];
     //狀態文字
-    public $Formal_Flag_Text = [
+    public array $Formal_Flag_Text = [
         1 => "非正式",
         2 => "正式",
+    ];
+    public array $Column_Title_Text = [
+        "ID" => "編號",
+        "Name" => "姓名",
+        "MemberNum" => "會員編號",
+        "UUID" => "第三方編號",
+        "Formal_Flag" => "狀態",
+        "Email" => "Email",
+        "Cellphone" => "手機",
+        "Tel" => "電話",
+        "Birthday" => "生日",
+        "LID" => "Line_ID",
+        "UID" => "FB_ID",
+        "Member_Level_ID" => "會員等級",
     ];
 }
