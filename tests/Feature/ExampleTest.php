db1d5e589a19ac19319bb67284f018585c54cb31
Bill <bill80362@gmail.com>
2023-08-29 20:40:46 +0800

    測試基本範本

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -8,12 +8,17 @@ use Tests\TestCase;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+     * 列表頁
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_member_data_list(): void
     {
-        $response = $this->get('/');
-
+        $response = $this->get('/Member_Data');
         $response->assertStatus(200);
+    }
+    //驗證是否擋住名稱必填
+    public function test_member_update_column_necessary(): void
+    {
+        $response = $this->post('/Member_Data/1',["Name"=>""]);
+        $response->assertStatus(302);
     }
 }
