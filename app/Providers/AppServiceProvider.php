749ca001bf2698a142f219a03e77d5c7c356584d
Bill <bill80362@gmail.com>
2023-09-10 12:42:25 +0800

    v1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,12 +11,18 @@ class AppServiceProvider extends ServiceProvider
 {
     /**
      * 不管是否被使用，都會直接先執行
+     * 根據SOLID的單一職責原則，register()只負責service container的register與binding，
+     * PS:
+     * 這邊很適合用來做客製化類別綁定，根據專案製作一個專屬Provider，後面被執行的會覆蓋前面的
      */
     public function register(): void
     {
         //
         $this->app->bind(SystemConfig::class, function ($app) {
-            return new SystemConfig();
+            return new SystemConfig("register1");
+        });
+        $this->app->bind(SystemConfig::class, function ($app) {
+            return new SystemConfig("register2");
         });
 //        $this->app->singleton(SystemConfig::class, function ($app) {
 //            return new SystemConfig();
@@ -24,12 +30,13 @@ class AppServiceProvider extends ServiceProvider
     }
 
     /**
-     * 被使用才呼叫
+     * boot()負責初始化物件。
      */
     public function boot(): void
     {
+        //會蓋掉原本的
 //        $this->app->singleton(SystemConfig::class, function ($app) {
-//            return new SystemConfig();
+//            return new SystemConfig("boot");
 //        });
     }
 }
--- app/Services/SystemConfig.php
+++ app/Services/SystemConfig.php
@@ -6,11 +6,11 @@ class SystemConfig
 {
     public string $TestConfigData;
 
-    public function __construct()
+    public function __construct($T)
     {
         //這邊看跑幾次
 //        echo "@@@";
 //        //從DB提取變數
-        $this->TestConfigData = "IamTesting";
+        $this->TestConfigData = $T;
     }
 }
