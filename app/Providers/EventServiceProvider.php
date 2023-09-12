c2168600f29681be87c519a2a4f8978771721e4b
bill <bill80362@gmail.com>
2023-09-12 17:33:35 +0800

    v1

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Events\PodcastProcessed;
+use App\Listeners\SendPodcastNotification;
+use App\Listeners\UserLoginListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
