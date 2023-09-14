aa20299cbf3325ec3b3e4f00e523c26981475d76
samchentw <89454932+samchentw@users.noreply.github.com>
2023-09-14 17:10:15 +0800

    update db

---
+++ app/Models/Permission/Perms.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models\Permission;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Perms extends Model
+{
+    use HasFactory;
+    
+    public $incrementing = true;
+    protected $keyType = 'int';
+    // protected $table = 'table name';
+    protected $guarded = [];
+
+    //事件偵測 retrieved, creating, created, updating, updated, saving, saved, deleting, deleted, restoring, restored
+    protected $dispatchesEvents = [
+        // 'saving' => '',
+        // 'created' => '',
+        // 'updated' => '',
+    ];
+}
