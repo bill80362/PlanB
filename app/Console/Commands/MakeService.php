61ce358babbe1e702495d40e634cfa7548f76d59
samchentw <89454932+samchentw@users.noreply.github.com>
2023-09-14 10:20:54 +0800

    建立make service command

---
+++ app/Console/Commands/MakeService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Foundation\Console\ModelMakeCommand;
+
+class MakeService extends GeneratorCommand
+{
+
+    /**
+     * @example php artisan make:service Payment/LinePayService     
+     * @var string
+     */
+    protected $signature = 'make:service {name}';
+
+ 
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '指令建立 service class';
+
+     /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Service';
+
+    /**
+     * Determine if the class already exists.
+     *
+     * @param  string  $rawName
+     * @return bool
+     */
+    protected function alreadyExists($rawName)
+    {
+        return class_exists($rawName);
+    }
+
+     /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'./stubs/service.stub';
+    }
+
+     /**
+     * Get the default namespace for the class.
+     *
+     * @param string $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return is_dir(app_path('Services')) ? $rootNamespace.'\\Services' : $rootNamespace;
+    }
+    
+}
