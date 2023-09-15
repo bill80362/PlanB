4820195abfe206c5da2b7c9728975259151fe7c0
samchentw <89454932+samchentw@users.noreply.github.com>
2023-09-15 16:56:47 +0800

    切換命名空間

--- routes/web/operate.php
+++ routes/web/operate.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\App;
 /**後台*/
 Route::prefix('/operate')->group(function () {
     Route::middleware(["AdminLoginAuth"])->group(function () {
-        Route::get('/', [\App\Http\Controllers\Admin\IndexController::class, "index"]);
+        Route::get('/', [\App\Http\Controllers\Operation\IndexController::class, "index"]);
 
         //管理員
         Route::get('/user', [\App\Http\Controllers\Operation\UserController::class, "listHTML"])->name("user_list");
@@ -17,20 +17,20 @@ Route::prefix('/operate')->group(function () {
         Route::get('/user/export', [\App\Http\Controllers\Operation\UserController::class, 'export']);
 
         //Dashboard
-        Route::get('/dashboard', [\App\Http\Controllers\Admin\IndexController::class, "indexHTML"])
+        Route::get('/dashboard', [\App\Http\Controllers\Operation\IndexController::class, "indexHTML"])
             // ->middleware(['can:memberLevel.read'])
         ;
 
         //Member_Data
-        Route::get('/member_data', [\App\Http\Controllers\Admin\MemberDataController::class, "listHTML"]);
-        Route::get('/member_data/{id}', [\App\Http\Controllers\Admin\MemberDataController::class, "updateHTML"])->whereNumber("id");
-        Route::post('/member_data/{id}', [\App\Http\Controllers\Admin\MemberDataController::class, "update"])->whereNumber("id");
-        Route::post('/member_data/del', [\App\Http\Controllers\Admin\MemberDataController::class, "delBatch"]);
-        Route::get('/member_data/export', [\App\Http\Controllers\Admin\MemberDataController::class, 'export']);
+        Route::get('/member_data', [\App\Http\Controllers\Operation\MemberDataController::class, "listHTML"]);
+        Route::get('/member_data/{id}', [\App\Http\Controllers\Operation\MemberDataController::class, "updateHTML"])->whereNumber("id");
+        Route::post('/member_data/{id}', [\App\Http\Controllers\Operation\MemberDataController::class, "update"])->whereNumber("id");
+        Route::post('/member_data/del', [\App\Http\Controllers\Operation\MemberDataController::class, "delBatch"]);
+        Route::get('/member_data/export', [\App\Http\Controllers\Operation\MemberDataController::class, 'export']);
     });
 
     //登入
-    Route::get('/login', [\App\Http\Controllers\Admin\LoginController::class, "loginHTML"]);
-    Route::post('/login', [\App\Http\Controllers\Admin\LoginController::class, "login"]);
-    Route::get('/logout', [\App\Http\Controllers\Admin\LoginController::class, "logout"]);
+    Route::get('/login', [\App\Http\Controllers\Operation\LoginController::class, "loginHTML"]);
+    Route::post('/login', [\App\Http\Controllers\Operation\LoginController::class, "login"]);
+    Route::get('/logout', [\App\Http\Controllers\Operation\LoginController::class, "logout"]);
 });
