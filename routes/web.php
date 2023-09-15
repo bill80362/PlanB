137749d8ed7923bc9adeb0a8714b668a8940a70e
samchentw <89454932+samchentw@users.noreply.github.com>
2023-09-15 11:50:16 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\App;
 
-include __DIR__ . '/web/front.php'; // 前台
+
 include __DIR__ . '/web/oper.php'; // 後台
 include __DIR__ . '/web/oauth.php'; // 前台
+include __DIR__ . '/web/front.php'; // 前台
 
 
