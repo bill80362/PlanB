0d82e9f53c09c5bd2bf8adba648c16e02b492475
bill <bill80362@gmail.com>
2023-09-19 11:11:26 +0800

    v1

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -12,7 +12,6 @@ class OrderController extends Controller
 
     public function queryOrder(Request $request)
     {
-        __("");
         $user = auth('erp')->user();
         // $test = User::where('id', 55)->firstOrFail(); //錯誤測試
         return [
