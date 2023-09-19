d89489bc366d32943d2b886323fae7749d9bae26
Bill <bill80362@gmail.com>
2023-09-19 22:38:14 +0800

    路由語系

--- routes/web/front.php
+++ routes/web/front.php
@@ -11,28 +11,3 @@ Route::group([
     Route::get('/', [\App\Http\Controllers\Front\HomepageController::class, 'index']);
     Route::get('/about', [\App\Http\Controllers\Front\HomepageController::class, 'about']);
 });
-
-//
-///**
-// * Post方法
-// */
-//Route::middleware(['lang'])->group(function () {
-//    Route::post('/login', [\App\Http\Controllers\Front\MemberController::class, 'login']);
-//    Route::post('/change-lang', [\App\Http\Controllers\Front\IndexController::class, 'changeLang']);
-//});
-//
-//
-///**
-// * Get方法
-// */
-//$systemConfigService = app(SystemConfigService::class);
-//$useLangPrefix = $systemConfigService->useLangPrefix;
-//
-//Route::middleware(['lang'])->prefix($useLangPrefix ? '{lang?}' : '')->group(function () {
-//    Route::middleware(['auth:web_front'])->group(function () {
-//        Route::get('/member', [\App\Http\Controllers\Front\MemberController::class, 'index']);
-//        Route::get('/logout', [\App\Http\Controllers\Front\MemberController::class, "logout"]);
-//    });
-//    Route::get('/http-test', [\App\Http\Controllers\Front\IndexController::class, 'httpTest']);
-//
-//});
