c91d7e2f15be1eb0460662fdbf3ef3cb68ed7213
bill <bill80362@gmail.com>
2023-09-20 10:18:59 +0800

    debugBar外掛

--- routes/web/front.php
+++ routes/web/front.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 
-//不強迫導轉範本，如果開頭語系成立，就吃語系，如果不是成立的語系，就使用預設語系，但是不強迫導轉
 Route::group([
-    "prefix" => app(\App\Services\RouteLanguageService::class)->setLangNoRedirect(),
+    //開啟語系偵測，如果開頭有語系就吃語系，如果開頭不是語系，就使用預設語系，但不強迫導轉
+    "prefix" => app(\App\Services\RouteLanguageService::class)->setLangNoRedirect(),//開啟語系偵測
 //    "middleware" => ["lang.redirect"],//開啟強迫導轉
 ],function () {
     Route::get('/', [\App\Http\Controllers\Front\HomepageController::class, 'index']);
