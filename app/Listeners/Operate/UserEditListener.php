6ab9a2ecae707d21a5e38e69e52eb0c06b687cad
bill <bill80362@gmail.com>
2023-09-22 18:30:44 +0800

    v1

--- app/Listeners/Operate/UserEditListener.php
+++ app/Listeners/Operate/UserEditListener.php
@@ -3,10 +3,25 @@
 namespace App\Listeners\Operate;
 
 use App\Events\Operate\UserEditEvent;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Log;
 
-class UserEditListener
+class UserEditListener implements ShouldQueue
 {
+    /**
+     * The name of the connection the job should be sent to.
+     *
+     * @var string|null
+     */
+//    public $connection = 'database';
+
+    /**
+     * The time (seconds) before the job should be processed.
+     *
+     * @var int
+     */
+//    public $delay = 3;
+
     /**
      * Create the event listener.
      */
@@ -22,6 +37,7 @@ class UserEditListener
     {
         //通知先使用LOG表達
         Log::channel('daily')->info(json_encode($event));
+//        echo 123;
 
     }
 }
