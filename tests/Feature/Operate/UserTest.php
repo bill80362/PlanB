12b8432552c6be13d509cef97bb2fc6f3e33bc42
samchentw <89454932+samchentw@users.noreply.github.com>
2023-09-23 16:17:38 +0800

    Update UserTest.php

--- tests/Feature/Operate/UserTest.php
+++ tests/Feature/Operate/UserTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 use Tests\TestCase;
+use App\Models\Permission\Permission;
 
 /**
  * 從route測試
@@ -40,27 +41,55 @@ class UserTest extends TestCase
     public function test_user_update_html(): void
     {
         $model = User::where('id', '!=', 1)->first();
-        $response = $this->get('/operate/user/'.$model->ID);
+        $response = $this->get('/operate/user/' . $model->ID);
         $response->assertStatus(200);
+    }
+
+
+
+    /**
+     * 更新
+     * 可驗證是否有正確寫入資料庫、測試加入權限
+     */
+    public function test_user_update_add_perm(): void
+    {
+        $name = 'admin12345678910';
+        $model = User::where('id', '!=', 1)->first();
+        $response = $this->post('/operate/user/' . $model->id, [
+            'name' => $name,
+            'email' => 'test@test.com',
+            'password' => Hash::make('1234567'),
+            'memberLevel_read' => 'on',
+        ]);
+        $checkUser = User::whereId($model->id)->first();
+
+        $permission = Permission::where('perm_key', 'memberLevel_read')->where('user_id', $model->id)->first();
+        $response->assertStatus(200);
+        $this->assertSame($name, $checkUser->name, '名字更新失敗'); //驗證姓名是否有正確寫入資料庫
+        $this->assertNotNull($permission, '權限沒有被加上');
     }
 
     /**
      * 更新
-     * 可驗證是否有正確寫入資料庫
+     * 測試移除權限
      */
-    public function test_user_update(): void
+    public function test_user_update_remove_perm(): void
     {
-        $name = 'admin12345678910';
         $model = User::where('id', '!=', 1)->first();
-        $response = $this->post('/operate/user/'.$model->id, [
-            'name' => $name,
+        Permission::create([
+            'user_id' => $model->id,
+            'perm_key' => 'memberLevel_read'
+        ]);
+
+        $response = $this->post('/operate/user/' . $model->id, [
+            'name' => $model->name,
             'email' => 'test@test.com',
             'password' => Hash::make('1234567'),
         ]);
-        $checkUser = User::whereId($model->id)->first();
 
+        $permission = Permission::where('perm_key', 'memberLevel_read')->where('user_id', $model->id)->first();
         $response->assertStatus(200);
-        $this->assertSame($name, $checkUser->name); //驗證姓名是否有正確寫入資料庫
+        $this->assertNull($permission, '權限沒有被移除');
     }
 
     /**
@@ -69,7 +98,7 @@ class UserTest extends TestCase
     public function test_user_update_necessary(): void
     {
         $model = User::where('id', '!=', 1)->first();
-        $response = $this->post('/operate/user/'.$model->id, ['name' => '', 'password' => '']);
+        $response = $this->post('/operate/user/' . $model->id, ['name' => '', 'password' => '']);
         $response->assertStatus(302); // 確認導回
         $this->assertTrue(Session::has('errors')); //確認有送錯誤訊息給前端
     }
