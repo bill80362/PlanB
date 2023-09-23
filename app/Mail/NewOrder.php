da8372f47582bc0c55abc6869e7d39f4bcbd24e4
samchentw <89454932+samchentw@users.noreply.github.com>
2023-09-23 11:17:30 +0800

    update mail

---
+++ app/Mail/NewOrder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NewOrder extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private Object $order;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(Object $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'New Order',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.orders.new_order',
+            with: [
+                'price' => $this->order->price
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
