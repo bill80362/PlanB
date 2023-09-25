3c55cb3d558881dfeda3b05fb20406a109891c4c
samchentw <89454932+samchentw@users.noreply.github.com>
2023-09-25 08:45:55 +0800

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,13 +4,11 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-use App\Services\Notify\LineNotifyService;
 
 class ExampleTest extends TestCase
 {
     public function test_example(): void
     {
-        $lineService = app(LineNotifyService::class);
         $this->assertTrue(true);
     }
 }
