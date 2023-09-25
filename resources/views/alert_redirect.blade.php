3682db8ee1abf3204eed91f09198aa8ba2bd803a
bill <bill80362@gmail.com>
2023-09-25 09:11:37 +0800

    v1

--- resources/views/alert_redirect.blade.php
+++ resources/views/alert_redirect.blade.php
@@ -1,15 +1,15 @@
-<html>
+{{--<html>--}}
 
-<body>
-    <script>
-        alert("{{ $Alert }}");
-        window.location.href = htmlDecode("{{ $Redirect }}");
-        function htmlDecode(input) {
-            var e = document.createElement('textarea');
-            e.innerHTML = input;
-            return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
-        }
-    </script>
-</body>
+{{--<body>--}}
+{{--    <script>--}}
+{{--        alert("{{ $Alert }}");--}}
+{{--        window.location.href = htmlDecode("{{ $Redirect }}");--}}
+{{--        function htmlDecode(input) {--}}
+{{--            var e = document.createElement('textarea');--}}
+{{--            e.innerHTML = input;--}}
+{{--            return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;--}}
+{{--        }--}}
+{{--    </script>--}}
+{{--</body>--}}
 
-</html>
+{{--</html>--}}
