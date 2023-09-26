433868f187f458eae66217e43d3d521f4719e511
samchentw <89454932+samchentw@users.noreply.github.com>
2023-09-26 09:32:06 +0800

    update

--- app/Http/Controllers/Operation/AuditController.php
+++ app/Http/Controllers/Operation/AuditController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Operation;
 use App\Http\Controllers\Controller;
 use App\Models\AuditLog;
 use App\Models\Permission\Permission;
+use App\Models\User;
 use App\Services\Operate\PermissionService;
 use App\Services\Operate\SystemConfigService;
 use Illuminate\Database\Eloquent\Collection;
@@ -52,7 +53,7 @@ class AuditController extends Controller
         }
         //輸入驗證遭擋，會有舊資料，優先使用舊資料
         foreach ((array) $this->request->old() as $key => $value) {
-            if (! $value) {
+            if (!$value) {
                 continue;
             }
             $Data->$key = $value;
@@ -80,7 +81,7 @@ class AuditController extends Controller
     {
         //過濾
         $UpdateData = $this->request->only(['name', 'email', 'password']);
-        if (! $UpdateData['password']) {
+        if (!$UpdateData['password']) {
             unset($UpdateData['password']);
         } else {
             $this->oModel->newPassword = $UpdateData['password'];
@@ -116,7 +117,7 @@ class AuditController extends Controller
         $this->oModel->find($id)->permissions()->saveMany($PermissionArray);
 
         //
-        return redirect("/operate/audit?".$this->request->getQueryString())->with(['success' => '送出成功']);
+        return redirect("/operate/audit?" . $this->request->getQueryString())->with(['success' => '送出成功']);
     }
 
     //批次刪除
@@ -127,14 +128,13 @@ class AuditController extends Controller
             $this->oModel->find($id)->delete();
         }
         //
-        return redirect("/operate/audit?".$this->request->getQueryString())->with(['success' => '刪除成功']);
+        return redirect("/operate/audit?" . $this->request->getQueryString())->with(['success' => '刪除成功']);
     }
 
     //批次修改排序
     public function sortBatch()
     {
         $ID_Array = $this->request->post('sort');
-
     }
 
     //批次還原
@@ -142,32 +142,32 @@ class AuditController extends Controller
     {
         foreach ((array) $this->request->post('id_array') as $id) {
             $Data = $this->oModel->find($id);
-            if($Data->event=="created"){
+            if ($Data->event == "created") {
                 //新增反向，就是要刪除
                 $object = new $Data->auditable_type;
                 $object->find($Data->new_values["id"])->delete();
-            }elseif($Data->event=="deleted"){
+            } elseif ($Data->event == "deleted") {
                 //刪除 => 新增
                 $object = new $Data->auditable_type;
-                //特殊表，要進行處裡
-                if($Data->auditable_type=="App\Models\User"){
-                    $Data->old_values = array_merge($Data->old_values,["password"=>"admin"]);
+                //特殊表，要進行處裡                
+                if ($Data->auditable_type == User::class) {
+                    $Data->old_values = array_merge($Data->old_values, ["password" => "admin"]);
                 }
                 //重新新增，id一樣看起來沒有問題
-//                unset($Data->old_values["id"]);
+                //                unset($Data->old_values["id"]);
                 //
                 $object->create($Data->old_values);
-            }elseif($Data->event=="updated"){
+            } elseif ($Data->event == "updated") {
                 //修改，
                 $object = $Data->auditable_type::find($Data->auditable_id);
-                foreach ($Data->old_values as $key => $value){
+                foreach ($Data->old_values as $key => $value) {
                     $object->$key = $value;
                 }
                 $object->save();
             }
         }
         //
-        return redirect("/operate/audit?".$this->request->getQueryString())->with(['success' => '還原成功']);
+        return redirect("/operate/audit?" . $this->request->getQueryString())->with(['success' => '還原成功']);
     }
 
     /**
@@ -183,11 +183,11 @@ class AuditController extends Controller
         foreach ((array) $subjects->toArray()[0] as $RowKey => $Row) {
             if ($RowKey > 0) {
                 break;
-            }//只跑第一行
+            } //只跑第一行
             foreach ($Row as $index => $columnTitle) {
                 //匯入資料欄位標題異常
-                if (! isset($value_to_key[$columnTitle])) {
-                    return redirect("/operate/audit?".$this->request->getQueryString())->with(['error' => '匯入標題異常']);
+                if (!isset($value_to_key[$columnTitle])) {
+                    return redirect("/operate/audit?" . $this->request->getQueryString())->with(['error' => '匯入標題異常']);
                 }
                 //
                 $excelIndex[$index] = $value_to_key[$columnTitle];
@@ -215,7 +215,7 @@ class AuditController extends Controller
             }
             //整理要更新的資料
             $DataModel = $this->oModel->importPrimary($UpdateData)->first();
-            if (! $DataModel) {
+            if (!$DataModel) {
                 $DataModel = clone $this->oModel; //沒有對應的資料，init一個
             }
             foreach ($UpdateData as $ColumnTitle => $value) {
@@ -233,7 +233,7 @@ class AuditController extends Controller
             //驗證有誤
             if ($validator->fails()) {
                 //
-                $AllMessage[] = "第{$RowKey}列:".implode(',', $validator->messages()->all());
+                $AllMessage[] = "第{$RowKey}列:" . implode(',', $validator->messages()->all());
 
                 //
                 continue;
@@ -245,7 +245,7 @@ class AuditController extends Controller
             return redirect()->back()->withErrors(['message' => implode(',', $AllMessage)]);
         }
         //
-        return redirect("/operate/audit?".$this->request->getQueryString())->with(['success' => '送出成功']);
+        return redirect("/operate/audit?" . $this->request->getQueryString())->with(['success' => '送出成功']);
     }
 
     //匯出
@@ -255,6 +255,6 @@ class AuditController extends Controller
         $ExportList = $this->oModel->filter($this->request->all())->export();
 
         //匯出
-        return (new Collection($ExportList))->downloadExcel('audit_data_'.time().'.xlsx');
+        return (new Collection($ExportList))->downloadExcel('audit_data_' . time() . '.xlsx');
     }
 }
