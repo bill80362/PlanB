72cfc1fe10408f5effbaf010d29fb7d3240ff6c9
samchentw <89454932+samchentw@users.noreply.github.com>
2023-09-26 15:29:29 +0800

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,12 @@ use Illuminate\Support\Str;
 
 class ExampleTest extends TestCase
 {
-    public function test_example(): void
+    /**
+     * 測試客製化驗證
+     */
+    public function test_rule(): void
     {
-        // $converted = Str::of('privacy_statement')->camel();
-        // dd($converted);
+
         $validator = Validator::make([
             'phone' => '0910585824'
         ], [
@@ -21,5 +23,31 @@ class ExampleTest extends TestCase
         ]);
         $check = $validator->passes();
         $this->assertTrue($check);
+    }
+
+    /**
+     * 時間相關測試，如：訂單幾天後應結案、到貨幾天應發送訊息等等
+     * @link https://laravel.com/docs/10.x/mocking#interacting-with-time
+     */
+    public function test_time(): void
+    {
+        /**
+         * 建立訂單
+         */
+        // craeteOrder() 
+
+        $this->travel(10)->days(function () {
+            /**
+             * 訂單應在10天結案
+             */
+            // overOrder()
+
+            /**
+             * 撈db確認訂單是否已結案
+             */
+        });
+
+
+        $this->assertTrue(true);
     }
 }
