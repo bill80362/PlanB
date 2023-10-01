69c75b32b605bb74bd04dd0c3cbfb1736b47c4db
Bill <bill80362@gmail.com>
2023-10-01 18:35:47 +0800

    v1

--- resources/views/operate/include/_Aside.blade.php
+++ resources/views/operate/include/_Aside.blade.php
@@ -3,7 +3,7 @@
 <!-- sidebar  -->
 <nav class="sidebar vertical-scroll  ps-container ps-theme-default ps-active-y">
     <div class="logo d-flex justify-content-between">
-{{--        <a href="index.html"><img src="/template/Salessa/img/logo.png" alt=""></a>--}}
+        <a href="index.html"><img src="/template/Salessa/img/logo.png" alt=""></a>
         <div class="sidebar_close_icon d-lg-none">
             <i class="ti-close"></i>
         </div>
