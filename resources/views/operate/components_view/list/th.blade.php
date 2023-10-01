4415defa0f70405897c8f2fdc3c14ab6757c77bb
Bill <bill80362@gmail.com>
2023-10-01 18:35:27 +0800

    v1

--- resources/views/operate/components_view/list/th.blade.php
+++ resources/views/operate/components_view/list/th.blade.php
@@ -1,5 +1,5 @@
 <th class="{{$className}}"
     data-column="{{$column}}"
     onclick="{{$clickString}}" >
-    {{$model->Column_Title_Text[$column]??$column}}
+    {{__($model->Column_Title_Text[$column]??$column)}}
 </th>
