bb34337c97a972489e909444dc19cf31fe6c48a5
bill <bill80362@gmail.com>
2023-10-02 10:23:54 +0800

    v1

---
+++ app/View/Components/OperateFilterChosen.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class OperateFilterChosen extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(public $model){}
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        //
+        $chosenFilterList = [];
+        foreach( request()->all() as $filter_name => $filter_value){
+            //欄位名稱
+            $column = str_replace("filter_","",$filter_name);
+            //樣板
+            foreach( $this->model->filterTemplate as $useTemplateFilter => $template){
+                if($template=="rangeDate" && $column==$useTemplateFilter."_start"){
+                    $chosenFilterList[] = [
+                        "key" => $filter_name,
+                        "value" => $filter_value,
+                        "title" => $useTemplateFilter,
+                        "titleValue" => $filter_value.__("起"),
+                    ];
+                }elseif($template=="rangeDate" && $column==$useTemplateFilter."_end"){
+                    $chosenFilterList[] = [
+                        "key" => $filter_name,
+                        "value" => $filter_value,
+                        "title" => $useTemplateFilter,
+                        "titleValue" => $filter_value.__("迄"),
+                    ];
+                }elseif($template=="select2" && is_array($filter_value) ){
+                    foreach( $filter_value as  $filter_value_sub){
+                        $chosenFilterList[] = [
+                            "key" => $filter_name,
+                            "value" => $filter_value_sub,
+                            "title" => $this->model->Column_Title_Text[$column]??"",
+                            "titleValue" => isset($Model->{$column."Text"})?$this->model->{$column."Text"}[$filter_value_sub]:$filter_value_sub,
+                        ];
+                    }
+                }
+            }
+
+        }
+        //
+        return view('operate.components_view.filter.chosen',["chosenFilterList" => $chosenFilterList,]);
+    }
+}
---
+++ resources/views/operate/components_view/filter/chosen.blade.php
@@ -0,0 +1,24 @@
+<p class="d-flex align-items-center flex-content-start fz-sm filter-string">
+    <span class="text-muted me-2">{{__("篩選器")}}：</span>
+    @foreach( $chosenFilterList as $chosenFilter)
+        <button class="btn btn-secondary me-2 btn-sm rounded-pill px-3" onclick="deleteFilter('{{$chosenFilter["key"]}}','{{$chosenFilter["value"]}}')">
+            {{$chosenFilter["title"]}}：{{$chosenFilter["titleValue"]}} <i class="ti-close"></i>
+        </button>
+    @endforeach
+</p>
+
+<script>
+    //刪除Filter
+    function deleteFilter(deleteFilterName,deleteFilterValue){
+        let queryString = '{{request()->getQueryString()}}';
+        let newQueryString = '';
+        queryString.split("&amp;").map(function(item){
+            let name = item.split("=")[0];
+            let value = item.split("=")[1];
+            if(  ! (name.indexOf(deleteFilterName)>=0 && value == deleteFilterValue)  ){
+                newQueryString += item+'&';
+            }
+        });
+        location.href = "?" + newQueryString;
+    }
+</script>
