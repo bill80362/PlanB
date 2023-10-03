fc43056bea78bf9ed3f39d06330866efa9a28d1e
samchentw <89454932+samchentw@users.noreply.github.com>
2023-10-03 14:55:15 +0800

    Revert "移除views/admin資料夾"
    This reverts commit 923064e0408aaf14a8e02d4a137baebf64d33303.

---
+++ resources/views/admin/include/_Nav.blade.php
@@ -0,0 +1,124 @@
+
+<!-- menu  -->
+<div class="container-fluid g-0">
+    <div class="row">
+        <div class="col-lg-12 p-0 ">
+            <div class="header_iner d-flex justify-content-between align-items-center">
+                <div class="sidebar_icon d-lg-none">
+                    <i class="ti-menu"></i>
+                </div>
+                <div class="serach_field-area d-flex align-items-center">
+                    <div class="search_inner">
+                        <form action="#">
+                            <div class="search_field">
+                                <input type="text" placeholder="Search here..." >
+                            </div>
+                            <button type="submit"> <img src="/template/Salessa/img/icon/icon_search.svg" alt=""> </button>
+                        </form>
+                    </div>
+                    <span class="f_s_14 f_w_400 ml_25 white_text text_white" >Apps</span>
+                </div>
+                <div class="header_right d-flex justify-content-between align-items-center">
+                    <div class="header_notification_warp d-flex align-items-center">
+                        <li>
+                            <a class="bell_notification_clicker nav-link-notify" href="#"> <img src="/template/Salessa/img/icon/bell.svg" alt="">
+
+                            </a>
+                            <!-- Menu_NOtification_Wrap  -->
+                            <div class="Menu_NOtification_Wrap">
+                                <div class="notification_Header">
+                                    <h4>Notifications</h4>
+                                </div>
+                                <div class="Notification_body">
+                                    <!-- single_notify  -->
+                                    <div class="single_notify d-flex align-items-center">
+                                        <div class="notify_thumb">
+                                            <a href="#"><img src="/template/Salessa/img/staf/2.png" alt=""></a>
+                                        </div>
+                                        <div class="notify_content">
+                                            <a href="#"><h5>Cool Marketing </h5></a>
+                                            <p>Lorem ipsum dolor sit amet</p>
+                                        </div>
+                                    </div>
+                                    <!-- single_notify  -->
+                                    <div class="single_notify d-flex align-items-center">
+                                        <div class="notify_thumb">
+                                            <a href="#"><img src="/template/Salessa/img/staf/4.png" alt=""></a>
+                                        </div>
+                                        <div class="notify_content">
+                                            <a href="#"><h5>Awesome packages</h5></a>
+                                            <p>Lorem ipsum dolor sit amet</p>
+                                        </div>
+                                    </div>
+                                    <!-- single_notify  -->
+                                    <div class="single_notify d-flex align-items-center">
+                                        <div class="notify_thumb">
+                                            <a href="#"><img src="/template/Salessa/img/staf/3.png" alt=""></a>
+                                        </div>
+                                        <div class="notify_content">
+                                            <a href="#"><h5>what a packages</h5></a>
+                                            <p>Lorem ipsum dolor sit amet</p>
+                                        </div>
+                                    </div>
+                                    <!-- single_notify  -->
+                                    <div class="single_notify d-flex align-items-center">
+                                        <div class="notify_thumb">
+                                            <a href="#"><img src="/template/Salessa/img/staf/2.png" alt=""></a>
+                                        </div>
+                                        <div class="notify_content">
+                                            <a href="#"><h5>Cool Marketing </h5></a>
+                                            <p>Lorem ipsum dolor sit amet</p>
+                                        </div>
+                                    </div>
+                                    <!-- single_notify  -->
+                                    <div class="single_notify d-flex align-items-center">
+                                        <div class="notify_thumb">
+                                            <a href="#"><img src="/template/Salessa/img/staf/4.png" alt=""></a>
+                                        </div>
+                                        <div class="notify_content">
+                                            <a href="#"><h5>Awesome packages</h5></a>
+                                            <p>Lorem ipsum dolor sit amet</p>
+                                        </div>
+                                    </div>
+                                    <!-- single_notify  -->
+                                    <div class="single_notify d-flex align-items-center">
+                                        <div class="notify_thumb">
+                                            <a href="#"><img src="/template/Salessa/img/staf/3.png" alt=""></a>
+                                        </div>
+                                        <div class="notify_content">
+                                            <a href="#"><h5>what a packages</h5></a>
+                                            <p>Lorem ipsum dolor sit amet</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="nofity_footer">
+                                    <div class="submit_button text-center pt_20">
+                                        <a href="#" class="btn_1">See More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--/ Menu_NOtification_Wrap  -->
+                        </li>
+                        <li>
+                            <a class="CHATBOX_open nav-link-notify" href="#"> <img src="/template/Salessa/img/icon/msg.svg" alt="">   </a>
+                        </li>
+                    </div>
+                    <div class="profile_info">
+                        <img src="/template/Salessa/img/client_img.png" alt="#">
+                        <div class="profile_info_iner">
+                            <div class="profile_author_name">
+                                <p>Neurologist </p>
+                                <h5>Dr. Robar Smith</h5>
+                            </div>
+                            <div class="profile_info_details">
+                                <a href="#">My Profile </a>
+                                <a href="#">Settings</a>
+                                <a href="#">Log Out </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/admin/layout/_MasterNoAuth.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Sales</title>
+
+    <link rel="icon" href="/template/Salessa/img/logo.png" type="image/png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/template/Salessa/css/bootstrap.min.css" />
+    <!-- themefy CSS -->
+    <link rel="stylesheet" href="/template/Salessa/vendors/themefy_icon/themify-icons.css" />
+    <!-- select2 CSS -->
+    <link rel="stylesheet" href="/template/Salessa/vendors/niceselect/css/nice-select.css" />
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="/template/Salessa/vendors/owl_carousel/css/owl.carousel.css" />
+    <!-- gijgo css -->
+    <link rel="stylesheet" href="/template/Salessa/vendors/gijgo/gijgo.min.css" />
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="/template/Salessa/vendors/font_awesome/css/all.min.css" />
+    <link rel="stylesheet" href="/template/Salessa/vendors/tagsinput/tagsinput.css" />
+
+    <!-- date picker -->
+    <link rel="stylesheet" href="/template/Salessa/vendors/datepicker/date-picker.css" />
+    <!-- scrollabe  -->
+    <link rel="stylesheet" href="/template/Salessa/vendors/scroll/scrollable.css" />
+    <!-- datatable CSS -->
+    <link rel="stylesheet" href="/template/Salessa/vendors/datatable/css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" href="/template/Salessa/vendors/datatable/css/responsive.dataTables.min.css" />
+    <link rel="stylesheet" href="/template/Salessa/vendors/datatable/css/buttons.dataTables.min.css" />
+    <!-- text editor css -->
+    <link rel="stylesheet" href="/template/Salessa/vendors/text_editor/summernote-bs4.css" />
+    <!-- morris css -->
+    <link rel="stylesheet" href="/template/Salessa/vendors/morris/morris.css">
+    <!-- metarial icon css -->
+    <link rel="stylesheet" href="/template/Salessa/vendors/material_icon/material-icons.css" />
+
+
+
+    <!-- menu css  -->
+    <link rel="stylesheet" href="/template/Salessa/css/metisMenu.css">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="/template/Salessa/css/style.css" />
+    <link rel="stylesheet" href="/template/Salessa/css/colors/default.css" id="colorSkinCSS">
+</head>
+<body class="crm_body_bg">
+
+
+<section class="dashboard_part large_header_bg">
+
+    <div class="main_content_iner ">
+        @yield('Content')
+    </div>
+
+    <!-- footer part -->
+    <div class="footer_part">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer_iner text-center">
+                        <p>2020 © Influence - Designed by <a href="#"> <i class="ti-heart"></i> </a><a href="#"> Dashboard</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- main content part end -->
+
+<div id="back-top" style="display: none;">
+    <a title="Go to Top" href="#">
+        <i class="ti-angle-up"></i>
+    </a>
+</div>
+
+<!-- footer  -->
+<script src="/template/Salessa/js/jquery-3.4.1.min.js"></script>
+<!-- popper js -->
+<script src="/template/Salessa/js/popper.min.js"></script>
+<!-- bootstarp js -->
+<script src="/template/Salessa/js/bootstrap.min.js"></script>
+<!-- sidebar menu  -->
+<script src="/template/Salessa/js/metisMenu.js"></script>
+<!-- waypoints js -->
+<script src="/template/Salessa/vendors/count_up/jquery.waypoints.min.js"></script>
+<!-- waypoints js -->
+<script src="/template/Salessa/vendors/chartlist/Chart.min.js"></script>
+<!-- counterup js -->
+<script src="/template/Salessa/vendors/count_up/jquery.counterup.min.js"></script>
+
+<!-- nice select -->
+<script src="/template/Salessa/vendors/niceselect/js/jquery.nice-select.min.js"></script>
+<!-- owl carousel -->
+<script src="/template/Salessa/vendors/owl_carousel/js/owl.carousel.min.js"></script>
+
+<!-- responsive table -->
+<script src="/template/Salessa/vendors/datatable/js/jquery.dataTables.min.js"></script>
+<script src="/template/Salessa/vendors/datatable/js/dataTables.responsive.min.js"></script>
+<script src="/template/Salessa/vendors/datatable/js/dataTables.buttons.min.js"></script>
+<script src="/template/Salessa/vendors/datatable/js/buttons.flash.min.js"></script>
+<script src="/template/Salessa/vendors/datatable/js/jszip.min.js"></script>
+<script src="/template/Salessa/vendors/datatable/js/pdfmake.min.js"></script>
+<script src="/template/Salessa/vendors/datatable/js/vfs_fonts.js"></script>
+<script src="/template/Salessa/vendors/datatable/js/buttons.html5.min.js"></script>
+<script src="/template/Salessa/vendors/datatable/js/buttons.print.min.js"></script>
+
+
+<script src="/template/Salessa/js/chart.min.js"></script>
+<!-- progressbar js -->
+<script src="/template/Salessa/vendors/progressbar/jquery.barfiller.js"></script>
+<!-- tag input -->
+<script src="/template/Salessa/vendors/tagsinput/tagsinput.js"></script>
+<!-- text editor js -->
+<script src="/template/Salessa/vendors/text_editor/summernote-bs4.js"></script>
+<script src="/template/Salessa/vendors/am_chart/amcharts.js"></script>
+
+<!-- scrollabe  -->
+<script src="/template/Salessa/vendors/scroll/perfect-scrollbar.min.js"></script>
+<script src="/template/Salessa/vendors/scroll/scrollable-custom.js"></script>
+
+
+<script src="/template/Salessa/vendors/chart_am/core.js"></script>
+<script src="/template/Salessa/vendors/chart_am/charts.js"></script>
+<script src="/template/Salessa/vendors/chart_am/animated.js"></script>
+<script src="/template/Salessa/vendors/chart_am/kelly.js"></script>
+<script src="/template/Salessa/vendors/chart_am/chart-custom.js"></script>
+<!-- custom js -->
+<script src="/template/Salessa/js/custom.js"></script>
+</body>
+</html>
